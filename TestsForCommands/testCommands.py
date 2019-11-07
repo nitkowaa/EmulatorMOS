51eac344a5baa0cceeb26d274491a88befa8d0aa
HubertLechowicz <40007858+HubertLechowicz@users.noreply.github.com>
2019-11-07 20:27:51 +0100

    Initial commit, simple test of a single command.
    Single test for the CMP command.

---
+++ TestsForCommands/testCommands.py
@@ -0,0 +1,14 @@
+import unittest
+x = 0
+
+def command(x):
+    x = 0  #Placeholder for more actual method
+
+class TestStringMethods(unittest.TestCase):
+
+
+    def test_commandCMP(self):
+        self.assertEqual('CMP',command(x))
+
+if __name__ == '__main__':
+    unittest.main()
