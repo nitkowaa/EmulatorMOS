aa97b1c875661d535f7d7473742610cc6dc92a51
nitkowaa <48529952+nitkowaa@users.noreply.github.com>
2019-12-09 22:03:02 +0100

    Merge branch 'WIP' of https://github.com/nitkowaa/EmulatorMOS
    # Conflicts:
    #	.idea/misc.xml

--- CPU/CPU.py
+++ CPU/CPU.py
@@ -1,16 +1,25 @@
 import numpy as np
 
+
 pamiec = np.random.randint(2, size=(8, 8))  # Two-dimensional array
+flagi = {"N" : 1, "Z" : 1,"C" : 1,"I" : 1,"D" : 1,"V" : 1}
+print("Flagi ",flagi)
+print(pamiec)
+print()
 
 akumulator = 0
 X = 0
 Y = 0
+
+#N Z C I D V
+
 
 pc = (np.random.randint(8,size=(2,1))) # musi mieć format macierzy 2 wymiarowej
 pc_x = pc[0][0]
 pc_y = pc[1][0]
 
 #Wczytaj miesjce z danego miejsca w pamieci do zmiennej Akumaltora
+
 
 def LDA(pc = pc,x = None,y = None):
     global akumulator
@@ -26,7 +35,10 @@ def LDA(pc = pc,x = None,y = None):
         akumulator = pamiec[pc_x][pc_y]
 
     print('akumulator: ',akumulator)
+
 #Wczytaj miesjce z danego miejsca w pamieci do zmiennej X
+
+
 def LDX(pc = pc,x = None,y = None):
     global X
     if x and y is not None:
@@ -39,7 +51,10 @@ def LDX(pc = pc,x = None,y = None):
         X = pamiec[pc_x][pc_y]
 
     print('X: ',X)
+
 #Wczytaj miesjce z danego miejsca w pamieci do zmiennej Y
+
+
 def LDY(pc = pc,x = None,y = None):
     global Y
     if x and y is not None:
@@ -55,6 +70,8 @@ def LDY(pc = pc,x = None,y = None):
 
 
 #Zapisz z Akumaltora do danego miejsca w pamieci do zmiennej
+
+
 def STA(x = None,y = None):
     global akumulator
     if x and y is not None:
@@ -68,6 +85,8 @@ def STA(x = None,y = None):
     akumulator = 0
     print('pamiec',pamiec[pc_x][pc_y],'akumulator',akumulator)
 #Zapisz z X do danego miejsca w pamieci do zmiennej
+
+
 def STX(x = None,y = None):
     global X
     if x and y is not None:
@@ -81,6 +100,8 @@ def STX(x = None,y = None):
     X = 0
     print('pamiec',pamiec[pc_x][pc_y],'Wartosc Y',X)
 #Zapisz z Y do danego miejsca w pamieci do zmiennej
+
+
 def STY(x = None,y = None):
     global Y
     if x and y is not None:
@@ -94,6 +115,7 @@ def STY(x = None,y = None):
     Y = 0
     print('pamiec',pamiec[pc_x][pc_y],'Wartosc X',Y)
 
+
 print('pc_x: ' ,pc_x)
 print('pc_y: ' ,pc_y)
 print('wartosc pamieci: ',pamiec[pc_x][pc_y])
@@ -101,6 +123,7 @@ print()
 print('TESTY:')
 print(akumulator,X,Y)
 LDA() #LDA
+
 LDX() #LDX
 LDY() #LDY
 print(akumulator,X,Y)
@@ -108,3 +131,15 @@ STA() #STA
 STX() #STX
 STY() #STY
 print(akumulator,X,Y)
+
+
+def CLD(): #zerowanie
+    global flagi
+    flagi.update(D="0")
+
+print()
+CLD() #CLD
+print()
+print("CLD start")
+print("CLD robi flagi" , flagi)
+
