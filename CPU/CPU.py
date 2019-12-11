cc468219aca9c97026a513fb0f1f67da75105b08
HubertLechowicz <40007858+hubertlechowicz@users.noreply.github.com>
2019-12-11 12:54:20 +0100

    Refactored entire project code. Changed folders structure for easier importing. Moved testing from CPU.py to a new Testy.py file. Prints no longer are outside methods due to code visibility going downhill Replaced CarryOnMyWaywardSon with a PROPER value name: CarryValue.

--- CPU/CPU.py
+++ CPU/CPU.py
@@ -4,14 +4,12 @@ import numpy as np
 pamiec = np.random.randint(2, size=(8, 8))  # Two-dimensional array
 flagi = {"N" : 0 ,"Z" : 0,"C" : 0,"I" : 0,"D" : 0,"V" : 0}
 
-print("Flagi ",flagi)
-print(pamiec)
-print()
+
 
 akumulator = 0
 X = 0
 Y = 0
-CarryOnMyWaywardSon = 0 #Reszta z Carry
+CarryValue = 0 #Zmienna przechowująca nadmiar liczby dodatniej
 #N Z C I D V
 
 
@@ -19,7 +17,8 @@ pc = (np.random.randint(8,size=(2,1))) # musi mieć format macierzy 2 wymiarowej
 pc_x = pc[0][0]
 pc_y = pc[1][0]
 
-#Wczytaj miesjce z danego miejsca w pamieci do zmiennej Akumaltora
+
+# Wczytaj miesjce z danego miejsca w pamieci do zmiennej Akumaltora
 
 
 def LDA(pc = pc,x = None,y = None):
@@ -37,7 +36,7 @@ def LDA(pc = pc,x = None,y = None):
 
     print('akumulator: ',akumulator)
 
-#Wczytaj miesjce z danego miejsca w pamieci do zmiennej X
+# Wczytaj miesjce z danego miejsca w pamieci do zmiennej X
 
 
 def LDX(pc = pc,x = None,y = None):
@@ -53,7 +52,7 @@ def LDX(pc = pc,x = None,y = None):
 
     print('X: ',X)
 
-#Wczytaj miesjce z danego miejsca w pamieci do zmiennej Y
+# Wczytaj miesjce z danego miejsca w pamieci do zmiennej Y
 
 
 def LDY(pc = pc,x = None,y = None):
@@ -70,7 +69,7 @@ def LDY(pc = pc,x = None,y = None):
     print('Y: ',Y)
 
 
-#Zapisz z Akumaltora do danego miejsca w pamieci do zmiennej
+# Zapisz z Akumaltora do danego miejsca w pamieci do zmiennej
 
 
 def STA(x = None,y = None):
@@ -85,7 +84,7 @@ def STA(x = None,y = None):
         pamiec[pc_x][pc_y] = akumulator
     akumulator = 0
     print('pamiec',pamiec[pc_x][pc_y],'akumulator',akumulator)
-#Zapisz z X do danego miejsca w pamieci do zmiennej
+# Zapisz z X do danego miejsca w pamieci do zmiennej
 
 
 def STX(x = None,y = None):
@@ -100,7 +99,7 @@ def STX(x = None,y = None):
         pamiec[pc_x][pc_y] = X
     X = 0
     print('pamiec',pamiec[pc_x][pc_y],'Wartosc Y',X)
-#Zapisz z Y do danego miejsca w pamieci do zmiennej
+# Zapisz z Y do danego miejsca w pamieci do zmiennej
 
 
 def STY(x = None,y = None):
@@ -117,106 +116,86 @@ def STY(x = None,y = None):
     print('pamiec',pamiec[pc_x][pc_y],'Wartosc X',Y)
 
 
-print('pc_x: ' ,pc_x)
-print('pc_y: ' ,pc_y)
-print('wartosc pamieci: ',pamiec[pc_x][pc_y])
-print()
-print('TESTY:')
-print(akumulator,X,Y)
-LDA() #LDA
-
-LDX() #LDX
-LDY() #LDY
-print(akumulator,X,Y)
-STA() #STA
-STX() #STX
-STY() #STY
-print(akumulator,X,Y)
-
-
-#Tu zaczynamy zerowanie flag
-def CLC(): #zerowanie C
+def CLC():  # zerowanie C
     global flagi
     flagi.update(C=0)
-
-print()
-CLC() #CLC
-print()
-print("CLC start")
-print("CLC robi flagi" , flagi)
+    print()
+    print("CLC zeruje flagi", flagi)
+    print()
 
 
-def CLD(): #zerowanie D
+
+
+
+def CLD():  # zerowanie D
     global flagi
     flagi.update(D=0)
-
+    print()
+    print("CLD zeruje flagi", flagi)
+    print()
 print()
-CLD() #CLD
-print()
-print("CLD start")
-print("CLD robi flagi" , flagi)
 
 
-def CLI(): #zerowanie I
+def CLI():  # zerowanie I
     global flagi
     flagi.update(I=0)
-
-print()
-CLI() #CLI
-print()
-print("CLI start")
-print("CLI robi flagi" , flagi)
+    print()
+    print("CLI zeruje flagi", flagi)
+    print()
 
 
 
-def CLV(): #zerowanie V
+
+def CLV():  # zerowanie V
     global flagi
     flagi.update(V=0)
-
-print()
-CLV() #CLV
-print()
-print("CLV start")
-print("CLV robi flagi" , flagi)
+    print()
+    print("CLV zeruje flagi" , flagi)
+    print()
 
 
-def SED(): #jedynkowanie D
+
+
+
+
+def SED():  # jedynkowanie D
     global flagi
     flagi.update(D=1)
-
-print()
-SED() #SED
-print()
-print("SED start")
-print("SED robi flagi" , flagi)
+    print()
+    print("SED ustawia flagi", flagi)
+    print()
 
 
-def SEC(): #jedynkowanie C
+
+
+
+def SEC():  # jedynkowanie C
     global flagi
     flagi.update(C=1)
-
-print()
-SEC() #SEC
-print()
-print("SEC start")
-print("SEC robi flagi" , flagi)
+    print()
+    print("SEC ustawia flagi", flagi)
+    print()
 
 
-def SEI(): #jedynkowanie I
+
+
+
+
+def SEI(): # jedynkowanie I
     global flagi
     flagi.update(I=1)
+    print()
+    print("SEI ustawia flagi", flagi)
 
-print()
-SEI() #SEI
-print()
-print("SEI start")
-print("SEI robi flagi" , flagi)
+
+
+
 
 
 def ADC(pc = pc, x = None, y = None):
     global akumulator
     global flagi
-    global CarryOnMyWaywardSon
+    global CarryValue
     if x and y is not None:
         akumulator = akumulator + pamiec[x][y] + flagi.get("C")
     elif x is not None:
@@ -235,7 +214,7 @@ def ADC(pc = pc, x = None, y = None):
     #Carry
     if akumulator >= 255 and flagi.get("N") == 0:
         flagi.update(C=1)
-        CarryOnMyWaywardSon = akumulator%255
+        CarryValue = akumulator%255
         akumulator = 255
 
     #Zero
@@ -261,21 +240,12 @@ def SBC(pc = pc, x = None, y = None):
     SEC()
     global akumulator
     if x and y is not None:
-        akumulator = akumulator - pamiec[x][y] - (255 - CarryOnMyWaywardSon)
+        akumulator = akumulator - pamiec[x][y] - (255 - CarryValue)
     elif x is not None:
-        akumulator = akumulator - pamiec[x][pc_y] - (255 - CarryOnMyWaywardSon)
+        akumulator = akumulator - pamiec[x][pc_y] - (255 - CarryValue)
     elif y is not None:
-        akumulator = akumulator - pamiec[pc_x][y] - (255 - CarryOnMyWaywardSon)
+        akumulator = akumulator - pamiec[pc_x][y] - (255 - CarryValue)
     else:
-        akumulator = akumulator - pamiec[pc_x][pc_y] - (255 - CarryOnMyWaywardSon)
+        akumulator = akumulator - pamiec[pc_x][pc_y] - (255 - CarryValue)
 
                                                                                                        # teraz piszemy ladnie
-print("Akumulator", akumulator)
-ADC()
-print("AKUMULATOR:", akumulator)
-akumulator = 300
-ADC()
-akumulator = 0
-print("po ADC",akumulator, CarryOnMyWaywardSon)
-SBC()
-print("po SBC", pamiec[pc_x][pc_y], akumulator)
