e0e646624a639ebe0d0ceb57f58adff771f9c763
xashotx <57191314+xashotx@users.noreply.github.com>
2019-12-16 21:16:09 +0100

    lista z rozkazami i zapisem

--- CPU/CPU.py
+++ CPU/CPU.py
@@ -3,10 +3,26 @@ import numpy as np
 pamiec = np.random.randint(2, size=(8, 8))  # Two-dimensional array
 flagi = {'N': 0, 'Z': 0, 'C': 0, 'I': 0, 'D': 0, 'V': 0}
 
-# Paweł = Przerobienie pamieci na liste (przepisanie), zmiennic nazwe indeksow pamieci z malego x,y na jakieś czytlne i lub j
-# Hubert = Przerobienie polecen na ich rozne warianty  (Rozkmienien jak działaja warianty)
-# Kamil = słownik z Tabelki poleceń (przepisanie)
-# Anita = Nowa polecenia w podstawowym wariancie (Rozkminienie)
+#Zapis MSD\LSD 13 strona w pdfie
+Rozkazy = \
+    {
+        "00": BRK(None), "01": ORA_indX(), "05": ORA_zpg(), "06": ASL_zpg(None), "08": PHP(None), "09": ORA_imm(), "0A": ASLA(None), "0D": ORA_abs(), "0E": ASL_abs(None),
+        "10": BPL_rel(None), "11": ORA_indY(), "15": ORA_zpgX(), "16": ASL_zpgX(None), "18": CLC(), "19": ORA_absY(), "1D": ORA_absX(), "1E": ASL_absX(None),
+        "20": JSR_abs(None), "21": AND_indX(), "24": BIT_zpg(None), "25": AND_zpg(), "26": ROL_zpg(None), "28": PLP(None), "2A": ROLA(None), "2C": BIT_abs(None),"2D": AND_abs(), "2E": ROL_abs(None),
+        "30": BMI_rel(None), "31": AND_indY(), "35": AND_zpgX(), "36": ROL_zpgX(None), "38": SEC(), "39": AND_absY(), "3D": And_absX(), "3E": ROL_absX(None),
+        "40": RTI(None), "41": EOR_indX(), "45": EOR_zpg(), "46": LSR_zpg(None), "48": PHA(None), "49": EOR_imm(), "4A": LSRA(None), "4C": JMP_abs(None), "4D": EOR_abs(), "4E": LSR_abs(None),
+        "50": BVC_rel(None), "51": EOR_indY(), "55": EOR_zpgX(), "56": LSR_zpgX(None), "58": CLI(), "59": EOR_absY(), "5D": EOR_absX(), "5E": LSR_absX(None),
+        "60": RTS(None), "61": ADC_indX(), "65": ADC_zpg(), "66": ROR_zpg(None), "68": PLA(None), "69": ADC_imm(), "6A": RORA(None), "6C": JMP_ind(None), "6D": ADC_abs(), "6E": ROR_abs(None),
+        "70": BVS_rel(None), "71": ADC_indY(), "75": ADC_zpgX(), "76": ROR_zpgX(None), "78": SEI(), "79": ADC_absY(), "7D": ADC_absX(), "7E": ROR_absX(None),
+        "81": STA_indX(), "84": STY_zpg(), "85": STA_zpg(), "86": STX_zpg(), "88": DEY(), "8A": TXA(None), "8C": STY_abs(), "8D": STA_abs() , "8E": STX_abs(),
+        "90": BCC_rel(None), "91": STA_indX(), "94": STY_zpgX(), "95": STA_zpgX(), "96": STX_zpgY(), "98": TYA(), "99": STA_absY(), "9A": TXS(None), "9D": STA_absX(),
+        "A0": LDY_imm(), "A1": LDA_indX(), "A2": LDX_imm(), "A4": LDY_zpg(), "A5": LDA_zpg(), "A6": LDX_zpg(), "A8": TAY(None), "A9": LDA_imm(), "AA": TAX(None), "AC": LDY_absX(), "AD": LDA_absX(), "AE": LDX_absY(),
+        "B0": BCS_rel(None), "B1": LDA_indY(), "B4": LDY_zpgX(), "B5": LDA_zpgX(), "B6": LDX_zpgY(), "B8": CLV(), "B9": LDA_absY(), "BA": TSX(None), "BC": LDY_absX(), "BD": LDA_absX(), "BE": LDX_absY(),
+        "C0": CPY_imm(None), "C1": CMP_indX(None), "C4": CPY_zpg(None), "C5": CMP_zpg(None), "C6": DEC_zpg(), "C8": INY(), "C9": CMP_imm(None), "CA": DEX(), "CC": CPY_abs(None), "CD": CMP_abs(None), "CE": DEC_abs(),
+        "D0": BNE_rel(None), "D1": CMP_indY(None), "D5": CMP_zpgX(), "D6": DEC_zpgX(), "D8": CLD(), "D9": CMPabsY(None), "DD": CMP_absX(None), "DE": DEC_abs(),
+        "E0": CPX_imm(None), "E1": SBC_indX(), "E4": CPX_zpg(None), "E5": SBC_zpg(), "E6": INC_zpg(), "E8": INX(), "E9": SBC_imm(), "EA": NOP(), "EC": CPX_abs(None), "ED": SBC_abs(), "EE": INC_abs(),
+        "F0": BEQ_rel(None), "F1": SBC_indY(), "F5": SBC_zpgX(), "F6": INC_zpgX(), "F8": SED(), "F9": SBC_absY(), "FD": SBC_absX(), "FE": INC_absX()
+    }
 
 akumulator = 0
 X = 0
@@ -314,98 +330,6 @@ def EOR():
         akumulator = 1
         flagi.update(Z=0)
         flagi.update(N=1)
-
-
-def JMP():  #skok do konkretnej instrukcji, pominięcie innych instrukcji, brak flag jakichkolwiek
-    global akumulator
-    global pamiec
-
-
-# Instrukcje branch zależą od flag
-
-flagi.update(N=1)
-
-def BCS():  # skok jeśli C=1
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('C') == 1:
-        print("hop")
-    else:
-        print("Nie hop")
-
-
-def BCC():  # skok jeśli C=0
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('C') == 0:
-        print("hop")
-    else:
-        print("Nie hop")
-
-
-def BEQ():  # skok jeśli Z=1
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('Z') == 1:
-        print("hop")
-    else:
-        print("Nie hop")
-
-
-def BNE():  # skok jeśli Z=0
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('Z') == 0:
-        print("hop")
-    else:
-        print("Nie hop")
-
-
-flagi.update(N=0)
-
-
-def BMI():  # skok jeśli N=1
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('N') == 1:
-        print("hop")
-    else:
-        print("Nie hop")
-BMI()
-
-def BPL():  # skok jeśli N=0
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('N') == 0:
-        print("hop")
-    else:
-        print("Nie hop")
-
-
-def BVS():  # skok jeśli V=1
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('V') == 1:
-        print("hop")
-    else:
-        print("Nie hop")
-
-
-def BVC():  # skok jeśli V=0
-    global akumulator
-    global pamiec
-    global flagi
-    if flagi.get('V') == 0:
-        print("hop")
-    else:
-        print("Nie hop")
 
 
 #endregion
