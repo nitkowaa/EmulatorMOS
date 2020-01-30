0992b330b76eb4b609f4eb1f30606013d3db20ea
Hubert Lechowicz <hubert.lechowicz@wir.pl>
2020-01-30 15:30:22 +0100

    Initial commit for GUI.

---
+++ CPU/GUI.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+import gui
+window = gui.Window("Hello")
+btn = window.add_button("Click Me")
+gui.on("btnPress", btn, lambda: print("Button Pressed."))
+#If you don't know what lambda is, it just returns an anonymous, single expression function.
+window.start()
